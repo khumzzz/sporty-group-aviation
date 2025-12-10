9d72f0c4108c6e6157b8258debaec0b6ee28a2a9
khumzzz <khumzzz@gmail.com>
2025-12-10 21:56:56 +0200

    initial commit

--- src/main/java/com/sportygroup/aviation/model/AirportDetailsApiResponse.java
+++ src/main/java/com/sportygroup/aviation/model/AirportDetailsApiResponse.java
@@ -2,7 +2,6 @@ package com.sportygroup.aviation.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sportygroup.aviation.contract.AirportDetailsStatus;
 import lombok.*;
 
 import java.io.Serializable;
